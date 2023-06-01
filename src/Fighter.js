d18d14677d0915b0cb25a2af8dd3c8ce719a5c05
Audrey Alaime <audreyalaime3@gmail.com>
2023-06-01 10:52:59 +0200

    Atelier n°2

--- src/Fighter.js
+++ src/Fighter.js
@@ -1,34 +1,43 @@
+const Weapon = require("./Weapon");
 const MAX_LIFE = 100;
 
 class Fighter {
-    constructor(name, strength, dexterity) {
-        this.name = name;
-        this.strength = strength;
-        this.dexterity = dexterity;
-        this.life = MAX_LIFE;
+  constructor(name, strength, dexterity) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+    this.weapon = null;
+  }
+
+  // Launch a fight
+  fight(defender) {
+    const attackPoints = this.getRandomInt(this.strength + this.getDamage());
+
+    const damages = Math.max(attackPoints - defender.dexterity, 0);
+
+    defender.life = Math.max(defender.life - damages, 0);
+  }
+
+  //Ajout des dommages de l'épée
+  getDamage() {
+    let totalDamage = this.strength;
+    if (this.weapon !== null) {
+      totalDamage += this.weapon.damage;
     }
+    return totalDamage;
+  }
 
+  // Generate a random value between 1 and max
+  getRandomInt(max) {
+    return 1 + Math.floor(Math.random() * max);
+  }
 
-    // Launch a fight
-    fight(defender) {
-        const attackPoints = this.getRandomInt(this.strength);
-
-        const damages = Math.max(attackPoints - defender.dexterity, 0);
-
-        defender.life = Math.max(defender.life - damages, 0);
-    }
-
-
-    // Generate a random value between 1 and max
-    getRandomInt(max) {
-        return 1 + Math.floor(Math.random() * max);
-    }
-
-
-    // Determine if a fighter is still alive
-    isAlive() {
-        return this.life > 0;
-    }
+  // Determine if a fighter is still alive
+  isAlive() {
+    return this.life > 0;
+  }
 }
 
 module.exports = Fighter;
+
